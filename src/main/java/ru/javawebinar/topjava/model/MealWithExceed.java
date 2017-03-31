b70cb43605b50faf8b3f513596a640ce7ed6ea08
vlad <fuentes.vlad@gmail.com>
2017-03-31 23:25:39 +0300

    HW(1.1) solved with colors

--- src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
@@ -7,6 +7,23 @@ import java.time.LocalDateTime;
  * 11.01.2015.
  */
 public class MealWithExceed {
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExceed() {
+        return exceed;
+    }
+
     private final LocalDateTime dateTime;
 
     private final String description;
