6bcdcde675e72cab949dc18c4ac77e59ab92f422
vlad <fuentes.vlad@gmail.com>
2017-04-20 17:54:48 +0300

    hw03 .01 (DB)

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -11,3 +12,10 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (dateTime, description, calories)
+VALUES ('2004-10-19 10:23:54', 'Завтрак', 500);
+;
+INSERT INTO meals (dateTime, description, calories)
+VALUES ('2013-5-19 11:00:00', 'Второй завтрак', 600);
+;
